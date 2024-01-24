a8fb5cc5bfd5bbc7f5fe6a26b1710e53a9421c26
Joy Agbo <joyagbo49@gmail.com>
2024-01-24 06:30:08 +0100

    Updated file to get and delete users

--- controller/userControlller.js
+++ controller/userControlller.js
@@ -1,6 +1,8 @@
+const { generateToken } = require("../config/jsonwebtk");
 const User = require("../models/userModel");
+const asyncHandler= require("express-async-handler");
 
-const createUser = async(req,res)=>{
+const createUser = asyncHandler (async(req,res)=>{
     const email = req.body.email;
     const findUser = await User.findOne({email:email})
     if(!findUser){
@@ -8,10 +10,62 @@ const createUser = async(req,res)=>{
         const newUser = await User.create(req.body)
         res.json(newUser)
     }else{
-        res.json({
-            status: "Fail",
-            message:"User Already existed"
-        })
+        throw new Error("User alreadly exist")
     }
-}
-module.exports={createUser }
+})
+
+const userLogin = asyncHandler(async (req, res)=>{
+    const {email, password} = req.body;
+    //check if user is in database
+    const findUser = await User.findOne({email});
+    if (findUser && (await findUser.isPasswordMatched(password))){
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id)
+        })
+    }else{
+        throw new Error("Invalid password")
+    }
+})
+//Get all users
+const getallUser = asyncHandler(async (req, res)=>{
+    try {
+        const getUsers = await User.find()
+        res.json(getUsers)
+    } catch (error) {
+      throw new Error(error)  
+    }
+});
+
+//get a one user
+const getaUser = asyncHandler(async(req, res)=>{
+    const { id } = req.params; 
+    try {
+       const getaUser = await User.findById(id);
+       res.json(getaUser)
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+//delete users
+const deleteaUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleteaUser = await User.findByIdAndDelete(id);
+        res.json(deleteaUser)
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+//Update Users
+module.exports={createUser, 
+    userLogin, 
+    getallUser, 
+    getaUser,
+    deleteaUser};
