2ddb3032b6e00edee2a3a159650d4455f102f27e
Joy Agbo <joyagbo49@gmail.com>
2024-01-24 05:50:54 +0100

    added role and password hashed before saving

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require("bcryptjs")
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     firstname:{
@@ -24,7 +24,18 @@ var userSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
+    role:{
+        type: String,
+        default: "user",  
+    }
 });
 
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSaltSync(10); 
+    this.password = await bcrypt.hash(this.password, salt) 
+})
+userSchema.methods.isPasswordMatched = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
 //Export the model
 module.exports = mongoose.model('User', userSchema);
