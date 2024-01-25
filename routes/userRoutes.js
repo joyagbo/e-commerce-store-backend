e774e0aed5af76b99079fd54e468be1146fd48f3
Joy Agbo <joyagbo49@gmail.com>
2024-01-25 06:22:56 +0100

    updated get a user enpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express= require('express');
 const { createUser, userLogin, getallUser, getaUser, deleteaUser, updateaUser } = require('../controller/userController');
+const {authMiddleware, isAdmin} = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login',userLogin);
 router.get('/all-users', getallUser);
-router.get('/:id', getaUser);
+router.get('/:id', authMiddleware,isAdmin, getaUser);
 router.delete('/:id', deleteaUser);
 router.put('/:id', updateaUser);
-module.exports = router
+module.exports = router;
