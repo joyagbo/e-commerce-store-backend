15aa8b62a609e045e1365639c578ff9c0a59b038
Joy Agbo <joyagbo49@gmail.com>
2024-01-25 05:17:06 +0100

    updated schema

--- models/userModel.js
+++ models/userModel.js
@@ -27,7 +27,15 @@ var userSchema = new mongoose.Schema({
     role:{
         type: String,
         default: "user",  
-    }
+    },
+    cart:{
+        type: Array,
+        default:[],
+    },
+    address:[{type: ObjectId, ref: "Address" }],
+    wishlist:[{type: ObjectId, ref:"Product"}],
+},{
+    timestamps: true
 });
 
 userSchema.pre('save', async function(next){
