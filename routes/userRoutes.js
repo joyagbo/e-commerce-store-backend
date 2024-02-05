9994499863cce7e0dffd7ca2c5b4700b074a9e44
Joy Agbo <joyagbo49@gmail.com>
2024-02-05 02:58:06 +0100

    added block and unblock routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,13 @@
 const express= require('express');
-const { createUser, userLogin, getallUser, getaUser, deleteaUser, updateaUser } = require('../controller/userController');
+const { createUser, 
+    userLogin, 
+    getallUser, 
+    getaUser, 
+    deleteaUser, 
+    updateaUser,
+    blockUser,
+    unblockUser
+ } = require('../controller/userController');
 const {authMiddleware, isAdmin} = require('../middlewares/authMiddleware');
 const router = express.Router();
 
@@ -8,5 +16,7 @@ router.post('/login',userLogin);
 router.get('/all-users', getallUser);
 router.get('/:id', authMiddleware,isAdmin, getaUser);
 router.delete('/:id', deleteaUser);
-router.put('/edit-user', authMiddleware, updateaUser);
+router.put('/edit-user/:id', authMiddleware, updateaUser);
+router.put('/block-user/:id', authMiddleware,isAdmin, blockUser);
+router.put('/unblock-user/:id', authMiddleware,isAdmin, unblockUser);
 module.exports = router;
