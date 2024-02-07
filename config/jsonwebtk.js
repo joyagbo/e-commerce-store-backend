1488377e4d95de22978976501843f5938a96baa4
joyagbo <joyagbo49@gmail.comgit config user.email joyagbo49@gmail.com>
2024-02-07 02:14:41 +0100

    added token expiration time

--- config/jsonwebtk.js
+++ config/jsonwebtk.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
 const generateToken = (id)=>{
-    return jwt.sign({id},process.env.SECRET_KEY,{expiresIn:"3days"})
+    return jwt.sign({id},process.env.SECRET_KEY,{expiresIn:"1d"})
 };
 module.exports= {generateToken};
